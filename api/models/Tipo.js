78930a1c52d3746ff23f8260ca46424adb32cefc
DezkaReid <elmaildeldezkareid@gmail.com>
2017-03-18 03:50:14 -0600

    Models pokemon and type added

--- api/models/Tipo.js
+++ api/models/Tipo.js
@@ -1,5 +1,5 @@
 /**
- * Pokemon.js
+ * Tipo.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
@@ -8,10 +8,9 @@
 module.exports = {
 
   attributes: {
-  	numero : 'integer',
   	nombre : 'string',
-  	tipo : {
-  		model : 'tipo',
+  	debilidad : {
+  		model : 'tipo'
   	}
   }
 };
